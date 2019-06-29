d9a392c276d118e97ece9aaf91274af7a39915df
manuel <manuel@Manuel>
2019-06-29 09:33:46 +0200

    I just have created the project

---
+++ spring_boot_crud_paginator/src/main/java/com/newtech/information/technology/app/SpringBootCrudPaginatorApplication.java
@@ -0,0 +1,13 @@
+package com.newtech.information.technology.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudPaginatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudPaginatorApplication.class, args);
+	}
+
+}
---
+++ spring_boot_crud_paginator/src/test/java/com/newtech/information/technology/app/SpringBootCrudPaginatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newtech.information.technology.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCrudPaginatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
